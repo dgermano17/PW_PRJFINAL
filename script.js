9694d3f9af63c63638a0b4d402133e0f0b5cb2d2
PRJFINAL <danilo.germano@etec.sp.gov.br>
2018-11-09 21:36:34 -0200

    Função btnSalvar: em andamento

---
+++ script.js
@@ -0,0 +1,9 @@
+var btnSalvar = document.querySelector("#btnSalvar");
+btnSalvar.addEventListener("click", function(evento){
+	event.preventDefault();
+	//console.log("OK");
+
+	var texto = document.querySelector("#texto").value;
+	console.log(texto);
+
+});
