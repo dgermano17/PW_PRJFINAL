e36a532c7ec14b79faa2ef1032efa4470dc53a48
PRJFINAL <danilo.germano@etec.sp.gov.br>
2018-11-09 22:17:05 -0200

    script.js; Funcçaõ

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+var form = document.querySelector("#form");
+
+function criarNota(cor,texto){
+	var container = document.querySelector("#container");
+	var notaD = document.createElement("div");
+	var notaP = document.createElement("p");
+
+	notaD.classlist.add(".nota");
+	notaD.style.backgroundColor = cor;
+	
+	notaP.textContent = texto;
+
+	notaD.appendChild(notaP);
+
+}
+
 var btnSalvar = document.querySelector("#btnSalvar");
 btnSalvar.addEventListener("click", function(evento){
 	event.preventDefault();
-	//console.log("OK");
+
+	criarNota(form.cor.value,form.texto.value);
 
 	var texto = document.querySelector("#texto").value;
-	console.log(texto);
+	var cor = document.querySelector("#cor").value;
 
+	var notaD = document.createElement("div");
+	var notaP = document.createElement("p");
+
+	notaD.appendChild(notaP);
 });
