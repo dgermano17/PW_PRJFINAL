ac07b60af62f5b0dcbb0384834ad4f9d2c7af0e2
Danilo <danilo.germano@etec.sp.gov.br>
2018-11-12 19:09:29 -0200

    1112 - CONTINUAÇÃO

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var form = document.querySelector("#form");
 function criarNota(cor,texto){
 	var container = document.querySelector("#container");
 	var notaD = document.createElement("div");
+	notaD.classlist.add(".nota");
 	var notaP = document.createElement("p");
 
-	notaD.classlist.add(".nota");
-	notaD.style.backgroundColor = cor;
 	
 	notaP.textContent = texto;
 
 	notaD.appendChild(notaP);
+	container.appendChild(notaD);
 
 }
 
@@ -18,13 +18,10 @@ var btnSalvar = document.querySelector("#btnSalvar");
 btnSalvar.addEventListener("click", function(evento){
 	event.preventDefault();
 
-	criarNota(form.cor.value,form.texto.value);
-
 	var texto = document.querySelector("#texto").value;
 	var cor = document.querySelector("#cor").value;
 
-	var notaD = document.createElement("div");
-	var notaP = document.createElement("p");
-
-	notaD.appendChild(notaP);
+	criarNota(form.cor.value,form.texto.value);
+	notaD.style.backgroundColor = form.cor.value;
+	
 });
