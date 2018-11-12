90d6e24e0e4dc2a18291a14f4cf2dbd88a3b05ca
Danilo <danilo.germano@etec.sp.gov.br>
2018-11-12 19:47:18 -0200

    MOSTRAR NOTA - Funcional

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 var form = document.querySelector("#form");
+var texto = document.querySelector("#texto").value;
+var cor = document.querySelector("#cor").value;
+var notaD;
+var notaP;
 
 function criarNota(cor,texto){
 	var container = document.querySelector("#container");
-	var notaD = document.createElement("div");
-	notaD.classlist.add(".nota");
-	var notaP = document.createElement("p");
+	notaD = document.createElement("div");
+	notaD.classList.add("nota");
+	notaP = document.createElement("p");
 
 	
 	notaP.textContent = texto;
-
 	notaD.appendChild(notaP);
 	container.appendChild(notaD);
-
 }
 
 var btnSalvar = document.querySelector("#btnSalvar");
 btnSalvar.addEventListener("click", function(evento){
 	event.preventDefault();
 
-	var texto = document.querySelector("#texto").value;
-	var cor = document.querySelector("#cor").value;
-
 	criarNota(form.cor.value,form.texto.value);
+
 	notaD.style.backgroundColor = form.cor.value;
-	
+
 });
